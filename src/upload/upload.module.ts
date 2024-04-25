71e82f087e9540b8321dedb6add148d67a07517f
Bruno Brizolara <bbrizolara7@gmail.com>
2024-04-25 13:28:03 -0300

    Upload file to s3 bucket & add rate limiter

---
+++ src/upload/upload.module.ts
@@ -0,0 +1,30 @@
+import { Module } from '@nestjs/common';
+import { UploadController } from './upload.controller';
+import { UploadService } from './upload.service';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ThrottlerModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => [
+        {
+          ttl: config.get('UPLOAD_RATE_TTL'),
+          limit: config.get('UPLOAD_RATE_LIMIT'),
+        },
+      ],
+    }),
+  ],
+  controllers: [UploadController],
+  providers: [
+    UploadService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
+})
+export class UploadModule {}
